ba625f46c8baf197642e333b1d1c3c58dac896b8
Benjamin Huth <benjamin.huth@physik.uni-regensburg.de>
2025-07-21 15:50:53 +0200

    updates

--- root_mpl_plot/impl/plotter.py
+++ root_mpl_plot/impl/plotter.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import math
+import textwrap
 
 import ROOT
 import atlasify
@@ -15,6 +16,7 @@ class Plotter:
         colors = matplotlib.colors.TABLEAU_COLORS
         del colors["tab:gray"]
         self.colors = colors.keys()
+        self.lw = 1.0
 
         self.ax_size = (6, 6)
 
@@ -26,8 +28,9 @@ class Plotter:
 
         fig.suptitle(name.replace("_", " "))
 
-        loaded = []
+        base_path = Path(options["base_path"] if "base_path" in options else "")
 
+        loaded = []
         for file_name, color in zip(files, self.colors):
             title = str(Path(file_name).name).replace(".root", "")
             loaded.append((file_name, title, color))
@@ -35,48 +38,62 @@ class Plotter:
         axes = [axes] if len(histograms) == 1 else axes.flatten()
         for hist, ax in zip(histograms, axes):
             for fname, label, color in loaded:
+                print(f"Load {fname}, {hist}", flush=True)
                 rfile = ROOT.TFile.Open(fname)
 
                 rebin_to = (
                     options["rebin"]["desired_bins"] if "rebin" in options else None
                 )
 
-                teff = Hist1DWrapper(rfile.Get(hist), rebin_to=rebin_to)
+                if "label_replace" in options:
+                    print(label)
+                    label = label.replace(*options["label_replace"])
+                    print(label)
+
+                teff = Hist1DWrapper(
+                    rfile.Get(str(base_path / hist)), rebin_to=rebin_to
+                )
                 teff.errorbar(
-                    ax, color=color, elinewidth=0.6, markersize=2, label=label, lw=0
+                    ax, color=color, elinewidth=self.lw, markersize=2, label=label, lw=0
                 )
                 rfile.Close()
 
                 print(f"Plotted: {fname}, {hist}, bins={teff.nbins}")
-                ax.set_title(hist.replace("_", " "))
+                ax.set_title("\n".join(textwrap.wrap(hist, 60)))
 
-        if "hlines" in options:
-            for ax in axes.flatten():
+        for ax in axes.flatten():
+            if "xlim" in options:
+                ax.set_xlim(*options["xlim"])
+
+            if "ylim" in options:
+                ax.set_ylim(*options["ylim"])
+
+            if "hlines" in options:
                 ax.hlines(
                     options["hlines"],
                     *ax.get_xlim(),
                     color="black",
-                    lw=0.6,
+                    lw=self.lw,
                     zorder=-100,
                 )
 
-        if "vlines" in options:
-            for ax in axes.flatten():
+            if "vlines" in options:
                 ax.vlines(
                     options["vlines"],
                     *ax.get_ylim(),
                     color="black",
-                    lw=0.6,
+                    lw=self.lw,
                     zorder=-100,
                 )
 
-        if "atlasify" in options:
-            atlasify_kwargs = dict(atlas="Simulation Internal")
-            atlasify_kwargs.update(options["atlasify"])
-            for ax in axes:
+            if "atlasify" in options:
+                atlasify_kwargs = dict(atlas="Simulation Internal")
+                atlasify_kwargs.update(options["atlasify"])
+
+                ax.legend(loc="lower right")
                 atlasify.atlasify(axes=ax, **atlasify_kwargs)
-        else:
-            for ax in axes.flatten():
+                atlasify.atlasify_legend(axes=ax)
+            else:
                 lmin, lmax = ax.get_ylim()
                 d = lmax - lmin
                 ax.set_ylim(lmin, lmax + d * 0.5)
